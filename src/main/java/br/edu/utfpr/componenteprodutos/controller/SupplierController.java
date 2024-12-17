0e9b3373df1fa4e722c15df22144d378d135cf47
Luan Felipe Marmentini <luanf2003@gmail.com>
2024-12-17 10:52:44 -0300

    refactor: Change URL supplier

--- src/main/java/br/edu/utfpr/componenteprodutos/controller/SupplierController.java
+++ src/main/java/br/edu/utfpr/componenteprodutos/controller/SupplierController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/supplier")
+@RequestMapping("/product/supplier")
 public class SupplierController {
 
     private final SupplierService supplierService;
